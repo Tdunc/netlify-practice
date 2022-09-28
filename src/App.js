7661582bd9265e06eb6f14252507bcdbff80c46b
Tdunc <tyson.121@hotmail.com>
2022-09-27 20:25:47 -0600

    worked

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React, { Component } from 'react';
-import { Form, Header, Container, List, Input, Segment } from 'semantic-ui-react';
+// import { Form, Header, Container, List, Input, Segment } from 'semantic-ui-react';
 import TranslatorWidget from 'react-translate-widget';
 
 class App extends Component {
   state = { name: '', todos: [] }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const { name, todos, } = this.state;
-    this.setState({ todos: [name, ...todos], name: ''})
-  }
+  
 
   render() {
-    const { name, todos } = this.state
+   
 
     return (
-      <Container>
+      <div>
         <TranslatorWidget sourceLanguageCode="en" className="translator"/>
-        <Segment textAlign='center'>
-          <Header as='h3' textAlign='center'>Todo List</Header>
-          <Form onSubmit={this.handleSubmit}>
-            <Input
-              required
-              value={name}
-              onChange={ e => this.setState({ name: e.target.value }) }
-            />
-          </Form>
-          <List>
-            { todos.map( (t, i) => <List.Item key={i}>{t}</List.Item> )}
-          </List>
-        </Segment>
-      </Container>
+        <h1>Hello Frenchie, my name is Bill</h1>
+      </div>
     );
   }
 }
